d96e44975bd97bd7e07a4403ed45347a8060d022
Mick McCarthy <mick@mccarthy.tech>
2020-08-02 23:25:40 +0100

    Bullet collisions

--- Weapons/Bullet.gd
+++ Weapons/Bullet.gd
@@ -4,5 +4,15 @@ const BULLET_SPEED : float = 2.5
 
 onready var forward: Vector3 = get_global_transform().basis[2].normalized() * -1
 
+func _ready() -> void:
+	connect('area_entered', self, '_on_area_entered')
+	connect('body_entered', self, '_on_body_entered')
+
 func _physics_process(delta: float) -> void:
 	translation += forward * BULLET_SPEED * delta
+
+func _on_area_entered(area: Area) -> void:
+	queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	queue_free()
